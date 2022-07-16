053374485f0a090446bd3df3fb661f82dbf2b5d0
Viacheslav <maverik2047@yandex.ru>
2022-07-16 20:06:38 +0300

    config added

--- src/test/java/ga/guru/owner/WebDriverTest.java
+++ src/test/java/ga/guru/owner/WebDriverTest.java
@@ -1,20 +1,40 @@
 package ga.guru.owner;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WebDriverTest {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void startDriver() {
+        driver = getDriver();
+    }
+
     @Test
-    public void testAmazon(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.amazon.com/");
-        String title =driver.getTitle();
-        assertEquals("Amazon.com. Spend less. Smile more.",title);
+    public void testAmazon() {
+
+        String title = driver.getTitle();
+        assertEquals("Amazon.com. Spend less. Smile more.", title);
+
+    }
+
+    @AfterEach
+    public void stopDriver() {
         driver.quit();
+    }
+//Конфигурация
+    private WebDriver getDriver() {
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.amazon.com/");
+        return driver;
     }
 }
